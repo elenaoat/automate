d6e114c66f585bbb1aae8a18c808921c174040f5
Elena Oat <oat.elena@gmail.com>
2014-04-11 09:42:04 +0300

    add additional lines for initial configuration of the servers

--- fresh_install.sh
+++ fresh_install.sh
@@ -3,6 +3,15 @@ apt-get update  # To get the latest package lists
 apt-get install nginx -y
 apt-get install python-pip -y
 apt-get install git -y
-sudo pip install python-twisted
+apt-get install python-dev -y
+apt-get install build-essential -y
+sudo add-apt-repository ppa:twisted-dev/ppa
+sudo pip install twisted
 sudo pip install autobahn
+####### LATER steps #############
 
+# perform vundle install + install nerdcommenter
+#change the mapleader in .vimrc: let mapleader="," for nerdcommenter
+
+#bind '"\C-p": shell-kill-word' #to make Ctrl+p remove a word in front of the cursor
+#sudo update-alternatives --config editor # to choose vim as default editor
